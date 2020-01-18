4c0bdbecd706e569488390f54fad62e41ebe6d63
strutte2 <shawnwwerner@gmail.com>
2020-01-17 23:48:05 -0600

    finished part 5

--- Python/django/tutorial/polls/views.py
+++ Python/django/tutorial/polls/views.py
@@ -1,33 +1,31 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader 
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone 
 
 from .models import Choice, Question
 
 # Create your views here.
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
+	def get_queryset(self):
+		"""Return the last five published questions."""
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {
-        'question': question
-    })
+class DetailView(generic.DetailView):
+	model = Question 
+	template_name = 'polls/detail.html'
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+class ResultsView(generic.DetailView):
+	model = Question 
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -46,7 +44,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
