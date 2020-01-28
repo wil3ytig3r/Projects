78837c8c38082ed0ab4469613c4d6adf02e4dc17
strutte2 <shawnwwerner@gmail.com>
2020-01-27 21:51:42 -0600

    shining_wheels

--- Ruby/ruby_tutorials/ri20min.rb
+++ Ruby/ruby_tutorials/ri20min.rb
@@ -1,33 +1,33 @@
 #!/usr/bin/env ruby
 
 class MegaGreeter
-attr_accessor :names
+	attr_accessor :names
 
-def initialize(names = "World")
-	@names = names 
-end
-
-def say_hi 
-	if @names.nil?
-		puts "..."
-	elsif @names.respond_to?("each")
-		@names.each do |name|
-			puts "Hello #{name}!"
-		end 
-	else 
-		puts "Hello #{@names}!" 
-	end 
-end 
-
-def say_bye 
-	if @names.nil?
-		puts "..."
-	elsif @names.respond_to?("join")
-		puts "Goodbye #{@names.join(", ")}. Come back soon!"
-	else 
-		puts "Goodbye #{@names}. Come back soon!"
+	def initialize(names = "World")
+		@names = names 
 	end
-end 
+
+	def say_hi 
+		if @names.nil?
+			puts "..."
+		elsif @names.respond_to?("each")
+			@names.each do |name|
+				puts "Hello #{name}!"
+			end 
+		else 
+			puts "Hello #{@names}!" 
+		end 
+	end 
+
+	def say_bye 
+		if @names.nil?
+			puts "..."
+		elsif @names.respond_to?("join")
+			puts "Goodbye #{@names.join(", ")}. Come back soon!"
+		else 
+			puts "Goodbye #{@names}. Come back soon!"
+		end
+	end 
 end 
 
 if __FILE__ == $0 
